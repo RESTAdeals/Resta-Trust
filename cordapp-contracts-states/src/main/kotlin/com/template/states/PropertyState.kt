ae726dbc0146670fe9eeb1a6ff3c629506d469eb
restaio <restaio@github.com>
2018-08-26 00:15:14 +0530

    Make Resta Trust skeleton

---
+++ cordapp-contracts-states/src/main/kotlin/com/template/states/PropertyState.kt
@@ -0,0 +1,23 @@
+package com.template.states
+
+import net.corda.core.contracts.Amount
+import net.corda.core.contracts.LinearState
+import net.corda.core.contracts.UniqueIdentifier
+import net.corda.core.identity.AbstractParty
+import java.util.*
+
+/**
+ *  State of the property that has to be stored on the main RESTA TRUST node.
+ *  So that any node can search for the available properties.
+ */
+data class PropertyState(
+            val name : String,               // Unique Name of the Property
+            val address : String,            // Address of the Property
+            val price : Amount<Currency>,    // Amount expects for investment
+            val owner : AbstractParty,       // Owner of the Property
+            val trustNode : AbstractParty,   // RESTA TRUST node
+            override val linearId: UniqueIdentifier = UniqueIdentifier()
+) : LinearState {
+    override val participants: List<AbstractParty> = listOf(owner, trustNode)      // Required signers.
+    // TODO : Add different aspects related to property
+}
---
+++ cordapp/src/main/kotlin/com/template/flows/InvestFlow.kt
@@ -0,0 +1,24 @@
+package com.template.flows
+
+import co.paralleluniverse.fibers.Suspendable
+import net.corda.core.flows.FlowLogic
+import net.corda.core.flows.InitiatingFlow
+import net.corda.core.flows.StartableByRPC
+
+@InitiatingFlow
+@StartableByRPC
+object InvestFlow {
+    class Initiator : FlowLogic<Unit>() {
+        @Suspendable
+        override fun call() {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+    }
+
+    class Responder : FlowLogic<Unit>() {
+        @Suspendable
+        override fun call() {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+    }
+}
